d2dcd8ba9de5c92f140cc8ed365240a99b96edc4
Matthew Downs <matthew.downsc@gmail.com>
2021-04-09 11:50:59 -0500

    Updating

--- src/types/Currency/Currency.test.ts
+++ src/types/Currency/Currency.test.ts
@@ -3,10 +3,17 @@ import FiatCurrencies from '~types/FiatCurrencies';
 import Currency from './Currency';
 
 describe('Currency', () => {
-    it('creates an ADA currency instance', () => {
-        const currency = new Currency(CryptoCurrencies.ADA);
+    it('creates a BTC currency instance', () => {
+        const currency = new Currency(CryptoCurrencies.BTC);
 
-        expect(currency.name).toBe('Cardano');
-        expect(currency.symbol).toBe('ADA');
+        expect(currency.name).toBe('Bitcoin');
+        expect(currency.symbol).toBe('BTC');
+    });
+
+    it('creates a USD currency instance', () => {
+        const currency = new Currency(FiatCurrencies.USD);
+
+        expect(currency.name).toBe('US Dollar');
+        expect(currency.symbol).toBe('USD');
     });
 })
